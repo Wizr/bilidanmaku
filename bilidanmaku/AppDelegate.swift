bbfc86cb08067c914eb1702e92fc680e3e64d9c0
Xudong Yang <fanderous@gmail.com>
2017-02-27 09:24:43 +0800

    Initial Commit

---
+++ bilidanmaku/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  bilidanmaku
+//
+//  Created by Xudong Yang on 27/02/2017.
+//  Copyright © 2017 Xudong Yang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ bilidanmaku/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  bilidanmaku
+//
+//  Created by Xudong Yang on 27/02/2017.
+//  Copyright © 2017 Xudong Yang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
